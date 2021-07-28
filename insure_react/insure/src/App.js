9d1557f94babbc3c9b2f2cf42745d445a3481b85
his7876 <his7876@daum.net>
2021-07-28 15:46:34 +0900

    react prac

--- insure_react/insure/src/App.js
+++ insure_react/insure/src/App.js
@@ -1,21 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import { useState } from "react";
+import ListPage from "./pages/ListPage";
+
+const Element = ({ username }) => {
+  return (
+    <>
+      <h1>안녕하세요 {username} 새로운 컴포넌트 입니다.</h1>
+    </>
+  );
+};
 
 function App() {
+  // let userInput = "test";
+  const [userInput, setUserInput] = useState("초기값");
+  const inputChange = (e) => {
+    const { value } = e.target;
+    console.log(value);
+    setUserInput(value);
+  };
+
+  const clickButton = () => {
+    console.log("사용자가 버튼을 클릭했습니다.");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> 안녕하세요 리액트 입니다. </p>
-        <a
-          className="App-link"
-          href="https://naver.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          네이버로 이동합니다
-        </a>
-      </header>
+    <div>
+      <Header title="테스트 페이지"></Header>
+      <Element username={"test"}></Element>
+      <b> 사용자 입력 값 : {userInput}</b>
+      <br />
+      <input onChange={inputChange}></input>
+      <button onClick={clickButton}>클릭 이벤트의 바인딩</button>
+      <ListPage></ListPage>
     </div>
   );
 }
