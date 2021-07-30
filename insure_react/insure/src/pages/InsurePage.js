32a1d3cbc8a3477565eededfde12c4402e222d71
his7876 <his30215@naver.com>
2021-07-30 16:05:09 +0900

    보험 조회 api + 비밀번호 암호화 완성

--- insure_react/insure/src/pages/InsurePage.js
+++ insure_react/insure/src/pages/InsurePage.js
@@ -6,9 +6,8 @@ const InsurePage = () => {
   const [result, setResult] = useState(undefined);
   const [searchInput, setsearchInput] = useState("");
   const [searchPWDInput, setsearchPWDInput] = useState("");
-  const [enMessage, setenMessage] = useState("");
-  
-  const [insureList, setinsureList] = useState([]);
+ 
+
   const handleSearchInput = (e) => {
     const { name , value } = e.target;
     if(name == "id"){
@@ -21,10 +20,7 @@ const InsurePage = () => {
   };
 
   const getInsureInfo = () => {
-    setenMessage(Cipher.makeAES(searchPWDInput));
-    console.log(enMessage);
-    console.log(searchInput);
-
+    
     const option = {
       method: "POST",
       url: "/scrap/pfm/credit4u/IntegratedInquiry",
@@ -34,7 +30,7 @@ const InsurePage = () => {
       },
       data: {
         LOGINID : searchInput,
-        LOGINPWD : enMessage
+        LOGINPWD : Cipher.makeAES(searchPWDInput)  //비밀번호 암호화 
       },
     };
     axios(option).then((response) => {
@@ -45,8 +41,11 @@ const InsurePage = () => {
   return (
     <div>
       아이디입력 : <input name = "id" onChange = {handleSearchInput}></input> <br/>
-      패스워드 입력: <input name = "pwd" onChange = {handleSearchInput}></input><br/>    
+      패스워드 입력 :{" "}
+      <input type="password" name="pwd" onChange={handleSearchInput}></input>
+      <br />
       <button onClick={getInsureInfo}>보험 조회하기</button>
+      {result !== undefined && <>{result.data.ERRMSG}</>}
     </div>
   );
 };
